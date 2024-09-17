87ac86910b30062cd9d68b52f8f6793c2fcf129f
Orly <76263061+Orly-Tian@users.noreply.github.com>
2024-09-18 01:08:35 +0800

    Update ex2_4.py

--- 第二章/ex2_4.py
+++ 第二章/ex2_4.py
@@ -9,13 +9,55 @@ import turtle as t
 
 
 # 叠加三角形
-for i in range(0, 3):
-    t.left(120)
-    t.fd(200)
+# for i in range(0, 3):
+#     t.left(120)
+#     t.fd(200)
+# t.left(120)
+# t.fd(100)
+# t.left(60)
+# t.fd(100)
+# for i in range(0, 2):
+#     t.left(120)
+#     t.fd(100)
+# t.done()
 
-t.left(120)
-t.fd(60)
-for i in range(0, 3):
-    t.fd(60)
-    t.left(120)
-t.done()
+
+# 无角正方形
+# for i in range(0, 4):
+#     t.penup()
+#     t.fd(50)
+#     t.pendown()
+#     t.fd(100)
+#     t.penup()
+#     t.fd(50)
+#     t.left(90)
+# t.done()
+
+
+# 六角形
+# t.left(30)
+# t.fd(200)
+# for i in range(0, 2):
+#     t.right(120)
+#     t.fd(200)
+# t.left(180)
+# t.fd(200 * 1/3)
+# t.left(120)
+# t.fd(200 * 2/3)
+# for i in range(0, 2):
+#     t.right(120)
+#     t.fd(200)
+# t.right(120)
+# t.fd(200 * 1/3)
+# t.done()
+
+
+# 螺旋正方形
+# t.speed(0)
+# t.left(90)
+# t.fd(300)
+# for i in range(1, 61):
+#     t.right(90)
+#     t.fd(300 - 5 * i)
+# t.done()
+
