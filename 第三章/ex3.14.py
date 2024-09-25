d462efc3f58feef048ef8076b58d62b77764a0e8
Orly <76263061+Orly-Tian@users.noreply.github.com>
2024-09-25 23:58:57 +0800

    Create ex3.14.py
    一年有365天，初始水平值为1.0，每工作一天水平增加N，不工作时水平不下降，一周连续工作6天，请编写程序运算结果填写下表

---
+++ 第三章/ex3.14.py
@@ -0,0 +1,18 @@
+# p78 一年有365天，初始水平值为1.0，每工作一天水平增加N，不工作时水平不下降，
+# 一周连续工作6天，请编写程序运算结果填写下表
+
+import numpy as np
+
+
+def calculate_dayup(dayfactor):
+    dayup = 1.0
+    for j in range(1, 365):
+        if not ((j % 7) in [0]):
+            dayup = dayup * (1 + dayfactor)
+    return dayup
+
+
+dayfactor = np.arange(0.001, 0.011, 0.001)
+
+for i in dayfactor:
+    print("%.2f" % calculate_dayup(i))
